d10196b6f126334dbcba0c0dc7daedbad6e03499
Zakariya <pzakariya@mail.ru>
2025-01-17 12:00:12 +0500

    Клавиатура теперь изменина на миниумм значение

--- keyboards/confirm.py
+++ keyboards/confirm.py
@@ -4,23 +4,5 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Каталог')],
     [KeyboardButton(text='Корзина'), KeyboardButton(text='Конатакты')]
-])
-
-<<<<<<< HEAD
-
-# def get_confirm_kb() -> InlineKeyboardMarkup:
-#     kb = InlineKeyboardBuilder()
-#     kb.button(text="Confirm", callback_data="confirm")
-#     return kb.as_markup()
-=======
-# main = ReplyKeyboardMarkup(keyboard=[
-#     [KeyboardButton(text='Каталог')],
-#     [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакты')]
-# ])
-#
-
-def get_confirm_kb() -> InlineKeyboardMarkup:
-    kb = InlineKeyboardBuilder()
-    kb.button(text="Confirm", callback_data="confirm")
-    return kb.as_markup()
->>>>>>> bba64e4530972c9895b7763a0bba57fcd7dd12e7
+], resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню.')
