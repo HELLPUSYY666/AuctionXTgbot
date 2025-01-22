c35eee10326ba5f6009881bdc9bc2d8e200358be
Zakariya <pzakariya@mail.ru>
2025-01-22 11:15:43 +0500

    Новая ветка аллах дал

--- keyboards/confirm.py
+++ keyboards/confirm.py
@@ -1,13 +1,18 @@
-from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Каталог')],
     [KeyboardButton(text='Корзина'), KeyboardButton(text='Конатакты')]
-])
+], resize_keyboard=True)
+
+settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='YouTube', url='https://www.youtube.com')]])
+
+cars = ['Tesla', 'Mercedes', 'BMW', 'Venus']
 
 
-# def get_confirm_kb() -> InlineKeyboardMarkup:
-#     kb = InlineKeyboardBuilder()
-#     kb.button(text="Confirm", callback_data="confirm")
-#     return kb.as_markup()
+async def inline_cars():
+    keyboard = ReplyKeyboardBuilder
+    for car in cars:
+        keyboard.add(KeyboardButton(text=car))
+    return keyboard.adjust(2)
